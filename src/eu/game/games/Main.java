693a1a0825b417f21a16184f1d59538cd4813b6e
GamInc <quentinsteclaire@gmail.com>
2015-04-02 18:40:06 +0200

    API Code
    THE CODE OF API

---
+++ src/eu/game/games/Main.java
@@ -0,0 +1,19 @@
+package eu.game.games;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+	
+	public void onEnable(){
+		System.out.println(" Starting API ! ");
+	}
+	
+	public void onDisable(){
+	System.out.println(" Starting API ! ");
+	}
+
+}
